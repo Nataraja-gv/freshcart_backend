38c5ff0c7d90dca5e4a5ddf1b56a1e574d6c367d
Nataraja-gv <nataraja2k@gmail.com>
2025-04-29 22:48:32 +0530

    commit

--- src/controllers/CategoryControllers.js
+++ src/controllers/CategoryControllers.js
@@ -1,3 +1,4 @@
+const productCategory = require("../models/productCategory");
 const ProductCategory = require("../models/productCategory");
 
 const CategoryController = async (req, res) => {
@@ -8,11 +9,11 @@ const CategoryController = async (req, res) => {
       return res.status(400).json({ message: "Category name is required" });
     }
 
-    const existingCategory = await ProductCategory.findOne({category_name});
-     
-     if(existingCategory){
-      return res.status(400).json({message:"category already exist"})
-     }
+    const existingCategory = await ProductCategory.findOne({ category_name });
+
+    if (existingCategory) {
+      return res.status(400).json({ message: "category already exist" });
+    }
     if (!req.file) {
       return res.status(400).json({ message: "Category image is required" });
     }
@@ -35,4 +36,18 @@ const CategoryController = async (req, res) => {
   }
 };
 
-module.exports = { CategoryController };
+const GetAllCategory = async (req, res) => {
+  try {
+    const categories = await productCategory.find();
+     res.status(200).json({
+      message: "All categories fetched successfully",
+      data: categories,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: error.message || "Failed to add category",
+    });
+  }
+};
+
+module.exports = { CategoryController,GetAllCategory  };
--- src/routers/categoryRouter.js
+++ src/routers/categoryRouter.js
@@ -1,13 +1,19 @@
 const express = require("express");
-const { CategoryController } = require("../controllers/CategoryControllers");
+const {
+  CategoryController,
+  GetAllCategory,
+} = require("../controllers/CategoryControllers");
 const upload = require("../middleware/multer");
 
 const router = express.Router();
 
 router.post(
   "/product/category/add",
-  upload.single("category_image"),  
+  upload.single("category_image"),
   CategoryController
 );
 
+router.get(
+  "/product/category/all",GetAllCategory
+);
 module.exports = router;
